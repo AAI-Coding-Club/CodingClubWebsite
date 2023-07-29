0f5d769add3badbc4ef4fbc4b6fdff52fc4a7c6f
DogeDevYT <52612977+DogeDevYT@users.noreply.github.com>
2023-07-29 16:14:47 -0400

    Updated Landing Page
    Updated Main Page with information about who we are and our image

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './styles/App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//VERY IMPORTANT TO IMPORT THIS FOR WEBPACK TO RECOGNIZE WE NEED THIS IMAGE
+import us from './images/us.png';
+
 function App() {
   return (
-    <div className='Centered'>
+    <div>
       <SiteNavBar />
-      <h1>Real Stuff Here</h1>
+      <div className='Centered'>
+        <h1>Welcome!</h1>
+        <h2>We are the Alliance Academy for Innovation's Coding Club!</h2>
+        <img src={us} alt='' className='image' />
+        <h2>The club plans to do many things for the 2023-2024 school year including but not limited to: Hackathons, Coding Workshops, STEM Boot Camps, and connecting members to Internship Oppurtunities!</h2>
+      </div>
     </div>
   );
 }
